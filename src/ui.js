124f199f3b6b8e727203e30b591b541c441db595
Ben Sharafian <benms64@gmail.com>
2016-12-12 11:33:36 +0100

    feat: add form to the UI

--- src/ui.js
+++ src/ui.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 module.exports = class UI {
   constructor ({ plugin, port }) {
     this._plugin = plugin
     this._port = port  
+
     this._app = express()
+    this._app.use(bodyParser.urlencoded({ extended: true }))
   }
 
-  _getHomepage () {
+  _getHomepage (content) {
     return `
 <html>
 <head>
@@ -22,10 +25,15 @@ module.exports = class UI {
 </head>
 <body>
   <div id="content">
-    <h1>Settlement Page</h1>
+    <h1><a style="color:black;" href="/">Settlement Page</a></h1>
     <hr />
     <br />
-    <p>Hello World!</p>
+    <form action="/" method="post">
+      <label>Amount <input type="number" name="amount" /></label><br>
+      <input type="submit" />
+    </form>
+    <br />
+    ${content || ''}
   </div>
 </body>
 </html>
@@ -34,7 +42,15 @@ module.exports = class UI {
 
   connect () {
     this._app.get('/', (req, res) => {
+      console.log('user requested "/"')
       res.send(this._getHomepage())
+    })
+
+    this._app.post('/', (req, res) => {
+      const amount = +req.body.amount
+
+      console.log('got settlement for ' + amount)
+      res.send(this._getHomepage('<p style="color:green;">Sent settlement of ' + amount + '</p>'))
     })
 
     return new Promise((resolve) => {
